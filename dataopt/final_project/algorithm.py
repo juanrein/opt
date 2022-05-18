9e271cfeb39e1081f78fdb3074bc58af2d41ef8e
Juha Reinikainen <juha.reinikainen1@gmail.com>
2022-05-18 18:12:50 +0300

    working repair

--- dataopt/final_project/algorithm.py
+++ dataopt/final_project/algorithm.py
@@ -9,11 +9,13 @@ from sklearn.utils import indices_to_mask
 
 
 class OneSumRepair(Repair):
-    def __init__(self, n_stocks) -> None:
+    def __init__(self, w_min, w_max, n_stocks) -> None:
         super().__init__()
+        self.w_min = w_min
+        self.w_max = w_max
         self.n_stocks = n_stocks
 
-    def _do(self, problem, pop, **kwargs):
+    def _do(self, problem, pop: Population, **kwargs):
         """
         Transforms weight vectors into feasible ones
         so that for each row the sum is 1
@@ -24,14 +26,20 @@ class OneSumRepair(Repair):
         X = pop.get("X")
         W = X[:, :self.n_stocks]
         Y = X[:, self.n_stocks:]
-        
-        W01 = W.clip(0, 1)
-        Wsum = W01.sum(axis=1)
-        Wnorm = W01.T / Wsum
+
+        Wminmax = W.clip(self.w_min, self.w_max)
+        #set weights of stocks that are not selected to zero
+        Wminmax = W * Y
+        Wsum = Wminmax.sum(axis=1)
+        Wnorm = Wminmax.T / Wsum
+
+        #Wsum can be zero if none stocks are chosen
+        nanValue = 1/self.n_stocks
+        Wnorm = np.nan_to_num(Wnorm, nan=nanValue)
+
         WnormY = np.hstack((Wnorm.T, Y))
         pop.set("X", WnormY)
         return pop
-
 
 def createInitialPopulation(population_size, n_stocks):
     W = np.random.random((population_size,n_stocks))
@@ -41,12 +49,13 @@ def createInitialPopulation(population_size, n_stocks):
     return Pr
 
 class BoundedRepair(Repair):
-    def __init__(self, w_min, w_max, c_min, c_max) -> None:
+    def __init__(self, w_min, w_max, c_min, c_max, n_stocks) -> None:
         super().__init__()
         self.w_min = w_min
         self.w_max = w_max
         self.c_min = c_min
         self.c_max = c_max
+        self.n_stocks = n_stocks
 
     def _do(self, problem, pop, **kwargs):
         """
@@ -61,21 +70,53 @@ class BoundedRepair(Repair):
         """
         X = pop.get("X")
 
+        W = X[:, :self.n_stocks]
+        Y = X[:, self.n_stocks:]
+
+        for i in range(len(W)):
+            cardinality = Y[i].sum()
+            #select more stocks to portfolio randomly to reach the minimum limit
+            if cardinality < self.c_min:
+                indicesOfZeros = [j for j in range(len(Y[i])) if Y[j] == 0]
+                howManyMissing = self.c_min - cardinality
+                clampI = np.random.choice(indicesOfZeros, howManyMissing, replace = False)
+                Y[i][clampI] = 1
+                W[i][clampI] = self.w_min
+                cardinality = Y[i].sum()
+            #select less stocks to portfolio to decrease to the maximum limit
+            if cardinality > self.c_max:
+                ones = [(j,W[i][j]) for j in range(len(Y[i])) if Y[j] == 1]
+                howManyTooMany = cardinality - self.c_max
+                onesSortedByWeight = sorted(ones, key=lambda x: x[1])
+                clampI = map(lambda x:x[0], onesSortedByWeight[:howManyTooMany])
+                Y[i][clampI] = 0
+                W[i][clampI] = 0
+
+
         # pop.set("X", Xnorm.T)
         
 
         return pop
 
 def main():
-    r = BoundedRepair(0.1, 0.9, 2, 3)
+    r = OneSumRepair(0.1, 0.9, 4)
     W = np.array([
-        [0.2, 0.4, 0.4, 0], #feasible
-        [0.2, 0.2, 0.3, 0.3], #too many
-        [1.0, 0, 0, 0], #too few
-        [0.05, 0.25, 0.7, 0], #loo low
-        [0.3, 0.4, 0.5, 0.6], #not 1
+        [0.2, 0.4, 0.4, 0.1],
+        [0.2, 0.2, 0.3, 0.3],
+        [0.6, 0.7, 0.5, 0.9],
+        [0.6, 0.7, 0.5, 0.9],
     ])
-    P = r.do(None, Population.create(W))
-    print(P.get("X"))
+    Y = np.array([
+        [1,1,0,1],
+        [0,1,1,0],
+        [1,1,1,1],
+        [0,0,0,0],
+    ], dtype=int)
+    X = np.hstack((W,Y))
 
-# main()
+    P = r.do(None, Population.create(X))
+    Res = P.get("X")
+    print(Res)
+    WW = Res[:,:4] * Res[:,4:]
+    print(WW)
+    print(WW.sum(axis=1))
